3b9e91dc0a2563af81522b4dc48e0110d08c047b
Tazkia Athariza Dhivara <tazkiaatharizadhivara@gmail.com>
2022-11-03 11:18:44 +0700

    integrated with API (login)

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,38 @@
-import React  from 'react';
+import React, { useState } from 'react';
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style.css';
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+
+
+  const [idUser, setIdUser] = useState('');
+  const token = localStorage.getItem('token');
+  const [validated, setValidated] = useState(false);
+  const direct = useNavigate();
+
+   const loginAccount = async (e) => {
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+
+    e.preventDefault();
+    setValidated(true);
+    /* await axios.post(
+      `https://todo-web-tbinar.herokuapp.com/login`,
+      {
+         idUser: idUser,
+       }
+     ); */
+    alert("Log In Successful!");
+    direct("/home")
+  };
 
   return (
     <Container className='login py-5'>
@@ -18,13 +45,15 @@ function Login() {
           </div>
          
          <div className="formLogin mt-4">
-          <form>
+          <form onSubmit={loginAccount}>
             <p className='judulForm' style={{backgroundColor:"white"}}>Use 4 digits of character</p>
             <input 
               className='inputLogin'
               maxlength="4"
               id="id_user"
               name="id_user"
+              value={idUser}
+              onChange={(e) => setIdUser(e.target.value)}
             />
             <button className='buttonLogin mt-3'>Log In</button>
           </form>
